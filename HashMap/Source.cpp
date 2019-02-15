3c4f23a76eafc3776985332809fcca945a00c89e
איתן ברקת <eitan.bareket@office.eduil.org>
2019-02-15 13:48:02 +0200

    constractor

--- HashMap/Source.cpp
+++ HashMap/Source.cpp
@@ -1,56 +1,10 @@
-#pragma once
-#include <string>
-#include <vector>
-#include <iostream>
+#include "hash_map.h"
 using namespace std;
 
-template <typename K, typename V>
-class HashMap
+int main(void)
 {
-public:
-	HashMap()
-	{
-		log("constructor");
-	}
-
-	~HashMap()
-	{
-		log("destructor");
-	}
-
-	HashMap(const HashMap& other)
-		: m_size(other.m_size))
-	{
-		log("copy constructor");
-		for (size_t i = 0; i < m_size; ++i)
-			m_data[i] = other.m_data[i];
-	}
-
-	Intvec& operator=(const Intvec& other)
-	{
-		log("copy assignment operator");
-		Intvec tmp(other);
-		std::swap(m_size, tmp.m_size);
-		std::swap(m_data, tmp.m_data);
-		return *this;
-	}
-private:
-	void log(const char* msg)
-	{
-		cout << "[" << this << "] " << msg << "\n";
-	}
-
-	K key;
-	V value;
-
-};
-
-int main(int argc, char** argv)
-{
-	Intvec v1(10); // [1,5,2,3,6]
-	Intvec v2;
-	v2 = v1;
-	//Intvec sorted = sort(v1);
+	HashMap<int, int> map;
+	map.insert(1, 2);
+	cout << map.get(1) << endl; // 2
+	map.print();
 }
-
-// sort the vector
--- HashMap/hash_map.h
+++ HashMap/hash_map.h
@@ -1,23 +1,85 @@
 #pragma once
 #include <string>
+#include <array>
 #include <vector>
-using namespace std;
+#include <iostream>
+const int MAX_SIZE = 128;
 
+
+template <typename K, typename V>
 class HashMap
 {
 public:
-	string key;
-	int value;
-
-	int size;
-
 	HashMap()
 	{
-		size = 0;
+		log("constructor");
 	}
 	~HashMap()
 	{
+		log("destructor");
 	}
-	
-	
+
+	void insert(K key, V value);
+	bool is_empty();
+	void print();
+	V get(K key);
+
+
+// implement best hashing
+
+/*
+	HashMap(const HashMap& other)
+		: m_size(other.m_size))
+	{
+	log("copy constructor");
+	for (size_t i = 0; i < m_size; ++i)
+		m_data[i] = other.m_data[i];
+	}
+
+	HashMap& operator=(const Intvec& other)
+	{
+		log("copy assignment operator");
+		Intvec tmp(other);
+		std::swap(m_size, tmp.m_size);
+		std::swap(m_data, tmp.m_data);
+		return *this;
+	}*/
+private:
+	void log(const char* msg)
+	{
+		std::cout << "[" << this << "] " << msg << std::endl;
+	}
+	std::array<std::vector<std::pair<K,V>>, MAX_SIZE> data;
+
 };
+
+
+template<typename K, typename V>
+void HashMap<K, V>::insert(K key, V value)
+{
+	// int key_index = hash(key);
+	int key_index = 0;
+	this->data[key_index].push_back({ key, value });
+}
+
+template<typename K, typename V>
+bool HashMap<K, V>::is_empty()
+{
+	return false;
+}
+
+template<typename K, typename V>
+void HashMap<K, V>::print()
+{
+	/*for (auto vec : this->data)
+	{
+		lo
+	}*/
+}
+
+template<typename K, typename V>
+V HashMap<K, V>::get(K key)
+{
+	// int key_index = hash(key);
+	// search for key in vector: this->data[key_index]
+}
